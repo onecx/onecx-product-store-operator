522c4cfdf53e58b70b3ddd6b1447f40f250fd287
Andrej Petras <andrej@lorislab.org>
2024-04-16 17:22:08 +0200

    feat: add product undeployed attribute (#29)

--- src/main/java/org/tkit/onecx/product/store/operator/client/mappers/ProductStoreMapper.java
+++ src/main/java/org/tkit/onecx/product/store/operator/client/mappers/ProductStoreMapper.java
@@ -1,6 +1,7 @@
 package org.tkit.onecx.product.store.operator.client.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.tkit.onecx.product.store.operator.ProductSpec;
 
 import gen.org.tkit.onecx.product.store.product.v1.model.UpdateProductRequest;
@@ -8,5 +9,6 @@ import gen.org.tkit.onecx.product.store.product.v1.model.UpdateProductRequest;
 @Mapper
 public interface ProductStoreMapper {
 
+    @Mapping(target = "undeployed", constant = "false")
     UpdateProductRequest map(ProductSpec spec);
 }
