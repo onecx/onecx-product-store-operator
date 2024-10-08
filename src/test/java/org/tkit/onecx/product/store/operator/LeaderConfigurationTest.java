107c93d858f82332bea01167cc6ac5d3085033e0
Andrej Petras <andrej@lorislab.org>
2024-10-08 17:18:57 +0200

    feat: add leader election configuration

---
+++ src/test/java/org/tkit/onecx/product/store/operator/LeaderConfigurationTest.java
@@ -0,0 +1,28 @@
+package org.tkit.onecx.product.store.operator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.product.store.test.AbstractTest;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class LeaderConfigurationTest extends AbstractTest {
+
+    @Inject
+    OperatorConfig dataConfig;
+
+    @Inject
+    LeaderConfiguration leaderConfiguration;
+
+    @Test
+    void testLeaderConfiguration() {
+        assertThat(dataConfig).isNotNull();
+        assertThat(dataConfig.leaderElectionConfig()).isNotNull();
+        assertThat(leaderConfiguration).isNotNull();
+        assertThat(leaderConfiguration.getLeaseName()).isNotNull().isEqualTo(dataConfig.leaderElectionConfig().leaseName());
+    }
+}
