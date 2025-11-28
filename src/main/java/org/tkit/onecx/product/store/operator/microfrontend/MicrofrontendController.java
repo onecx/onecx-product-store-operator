0377d3aeebecdc51ca00a498b7ca2c2035374dec
Jorden Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-11-28 08:25:12 +0100

    feat: increased test coverage and updated crd status access (#128)

--- src/main/java/org/tkit/onecx/product/store/operator/microfrontend/MicrofrontendController.java
+++ src/main/java/org/tkit/onecx/product/store/operator/microfrontend/MicrofrontendController.java
@@ -5,7 +5,7 @@ import jakarta.ws.rs.WebApplicationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.tkit.onecx.product.store.operator.CustomResourceStatus;
+import org.tkit.onecx.product.store.operator.AbstractResourceStatus;
 import org.tkit.onecx.product.store.operator.client.ProductStoreService;
 import org.tkit.onecx.quarkus.operator.OperatorUtils;
 
@@ -55,7 +55,7 @@ public class MicrofrontendController implements Reconciler<Microfrontend> {
         status.setRequestAppName(null);
         status.setRequestAppVersion(null);
         status.setResponseCode(responseCode);
-        status.setStatus(CustomResourceStatus.Status.ERROR);
+        status.setStatus(AbstractResourceStatus.Status.ERROR);
         status.setMessage(e.getMessage());
         microfrontend.setStatus(status);
         return ErrorStatusUpdateControl.patchStatus(microfrontend);
@@ -71,11 +71,11 @@ public class MicrofrontendController implements Reconciler<Microfrontend> {
         result.setResponseCode(responseCode);
         var status = switch (responseCode) {
             case 201:
-                yield CustomResourceStatus.Status.CREATED;
+                yield AbstractResourceStatus.Status.CREATED;
             case 200:
-                yield CustomResourceStatus.Status.UPDATED;
+                yield AbstractResourceStatus.Status.UPDATED;
             default:
-                yield CustomResourceStatus.Status.UNDEFINED;
+                yield AbstractResourceStatus.Status.UNDEFINED;
         };
         result.setStatus(status);
         microfrontend.setStatus(result);
