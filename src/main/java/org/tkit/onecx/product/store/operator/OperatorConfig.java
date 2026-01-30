6c2f498a4bda818894129747ce94a7cd85c47f49
Andrej Petras <andrej@lorislab.org>
2026-01-30 11:48:11 +0100

    fix: remove static init from configuration (#141)

--- src/main/java/org/tkit/onecx/product/store/operator/OperatorConfig.java
+++ src/main/java/org/tkit/onecx/product/store/operator/OperatorConfig.java
@@ -3,12 +3,10 @@ package org.tkit.onecx.product.store.operator;
 import io.quarkus.runtime.annotations.ConfigDocFilename;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
-import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
-@StaticInitSafe
 @ConfigDocFilename("onecx-product-store-operator.adoc")
 @ConfigMapping(prefix = "onecx.product-store.operator")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
